0b55eaa044731cef6a02c174ef104da5de303dd0
José Duarte <duarte.gmj@gmail.com>
2025-07-28 19:46:15 +0100

    Initial Commit

---
+++ DrunkenDice/DrunkenDiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrunkenDiceApp.swift
+//  DrunkenDice
+//
+//  Created by José Duarte on 28/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct DrunkenDiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
